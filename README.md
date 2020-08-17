46f7832b806efb8bcb695d60961a6ad3e2d0dab7
Adit Modi <adit.modi24@gmail.com>
2020-08-17 18:59:26 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Building-a-CI-CD-pipeline-for-a-containerized-Asp.Net-Core-3.0-Web-API
+# hello-pipelines
+Simple set of .Net Core 3.0 projects used to try out Azure Pipelines concepts.
+
+[![Build Status](https://dev.azure.com/julioc0382/hello-pipelines/_apis/build/status/julioct.hello-pipelines?branchName=master)](https://dev.azure.com/julioc0382/hello-pipelines/_build/latest?definitionId=1&branchName=master)
---
+++ Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WebApi.Controllers;
+using Xunit;
+
+namespace Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        private Mock<ILogger<WeatherForecastController>> loggerStub = new Mock<ILogger<WeatherForecastController>>();
+
+        [Fact]
+        public void Get_NoArguments_ReturnsDefaultForecastDays()
+        {
+            var controller = new WeatherForecastController(loggerStub.Object);
+            var expectedDays = 7;
+
+            var forecasts = controller.Get();
+
+            Assert.NotNull(forecasts);
+            Assert.Equal(expectedDays, forecasts.Count());
+        }
+    }
+}
